edce16fd267988bcea9b2692761e4aa5bb00574b
Alexandros Kolokas <40214710+AlexKolokas@users.noreply.github.com>
2021-03-16 13:54:39 +0200

    update inputs
    update so the user can give input of the text and the file name

--- qrgenerator.py
+++ qrgenerator.py
@@ -1,13 +1,22 @@
 import qrcode
-# Link for website
-input_data = "#write the link of your web site here or the text"
+# ask the user to give the informations for the qrcode
+print('Enter your site or text')
+input_data =input()
+
 #Creating an instance of qrcode
 qr = qrcode.QRCode(
         version=1,
         box_size=10,
         border=5,
         )
+
 qr.add_data(input_data)
 qr.make(fit=True)
-img = qr.make_image(fill='black', back_color='white')
-img.save('qrcode.png') #save the qr code as png
+#colors for the qrcode
+img = qr.make_image(fill_color='black', back_color='white')
+#ask the user to give the saving name of the qrcode
+print('Give the name for your file. exp:qrcode.png')
+inputfilename=input()
+#adds a .png to the saving name (so the qrcode is saved as png)
+inputfilenamefinal=inputfilename+'.png'
+img.save(inputfilenamefinal) #save the qr code as png
